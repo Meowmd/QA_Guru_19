0e2af2c8d72c04b4c0063084e0d86a82e88e6fa7
Mariya Bykova <bykova.m@gmail.com>
2022-10-14 15:00:48 +0700

    1 commit

---
+++ src/test/java/config/WebDriverConfig.java
@@ -0,0 +1,24 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources("classpath:config/remote.properties")
+
+public interface WebDriverConfig extends Config {
+
+    @Key("baseUrl")
+    @DefaultValue("http://demowebshop.tricentis.com")
+    String getBaseUrl();
+
+    @Key("browser")
+    @DefaultValue("chrome")
+    String getBrowser();
+
+    @Key("browserVersion")
+    @DefaultValue("100.0")
+    String getBrowserVersion();
+
+    @Key("remoteUrl")
+    String getRemoteUrl();
+
+}
